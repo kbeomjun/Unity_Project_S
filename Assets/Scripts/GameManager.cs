45f100cb5853180b4b3fcb5baf0fed1b65ace0e9
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2026-04-25 13:21:51 +0900

    상태 아이콘 변경

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -63,9 +63,9 @@ public class GameManager : MonoBehaviour
         TownRestManager.Instance.CardDeleteCoin = 100;
 
         _playerUnitDatas.Add(new UnitData(DataManager.Instance.UnitDatas[0]));
-        //_playerUnitDatas.Add(new UnitData(DataManager.Instance.UnitDatas[1]));
+        _playerUnitDatas.Add(new UnitData(DataManager.Instance.UnitDatas[1]));
         _playerUnitDatas.Add(new UnitData(DataManager.Instance.UnitDatas[2]));
-        //_playerUnitDatas.Add(new UnitData(DataManager.Instance.UnitDatas[3]));
+        _playerUnitDatas.Add(new UnitData(DataManager.Instance.UnitDatas[3]));
 
         _playerCardDatas.Add(new CardData(DataManager.Instance.CardDatas[0]));
         _playerCardDatas.Add(new CardData(DataManager.Instance.CardDatas[1]));
