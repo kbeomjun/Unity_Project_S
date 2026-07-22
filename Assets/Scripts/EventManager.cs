120fc9ebee11df4cf201855a68c525fd0d1b70b2
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2026-07-22 12:45:00 +0900

    녹화 세팅

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -24,40 +24,40 @@ public class EventManager : MonoBehaviour
     {
         _eventDescriptions = new List<string>()
         {
-            //"Encountered a wandering mercenary group...",
-            //"Discovered an ancient sanctuary deep in the forest. A faint light is emanating from the altar...",
-            //"Found a seriously injured man by the side of the forest path. A worn-out bag lay beside him. He appeared to still be alive.",
+            "Encountered a wandering mercenary group...",
+            "Discovered an ancient sanctuary deep in the forest. A faint light is emanating from the altar...",
+            "Found a seriously injured man by the side of the forest path. A worn-out bag lay beside him. He appeared to still be alive.",
             "Beneath a worn canvas tent, a shady man beckons you closer.\r\n\r\n\"Care for a game?\"",
         };
         _events = new List<GameEvent>()
         {
-            //new GameEvent
-            //(
-            //    new List<EventOption>
-            //    {
-            //        new EventOption("Sell supplies", "Gain 50 coin", new List<IEventEffect> { new GainCoin(50) }),
-            //        new EventOption("Plunder the mercenary group", "Gain 300 coin, Lose 20 health(All Party)", new List<IEventEffect> { new GainCoin(300), new HurtUnit(20, true) }),
-            //        CreateOption()
-            //    }
-            //),
-            //new GameEvent
-            //(
-            //    new List<EventOption>
-            //    {
-            //        new EventOption("Offer a prayer to God", "Increase defense by 5(All Party)", new List<IEventEffect> { new UpgradeDefense(5) }),
-            //        new EventOption("Offer tribute", "Pay 100 coin, Increase maxhealth by 5(All Party)", new List<IEventEffect> { new PayCoin(100), new UpgradeHealth(5) }),
-            //        new EventOption("Pledge a blood oath.", "Lose 10 health(All Party), Increase attack by 5(All Party)", new List<IEventEffect> { new HurtUnit(10, true), new UpgradeAttack(5) }),
-            //    }
-            //),
-            //new GameEvent
-            //(
-            //    new List<EventOption>
-            //    {
-            //        new EventOption("Heal it", "Pay 100 coin, Get 1 random card", new List<IEventEffect> { new PayCoin(100), new GetRandomCard(1) }),
-            //        new EventOption("Kill and plunder", "Decrease maxhealth by 5(All Party), Get 1 random card", new List<IEventEffect> { new DecreaseHealth(5), new GetRandomCard(1) }),
-            //        new EventOption("Abandon and leave", "Nothing happens", new List<IEventEffect> { }),
-            //    }
-            //),
+            new GameEvent
+            (
+                new List<EventOption>
+                {
+                    new EventOption("Sell supplies", "Gain 50 coin", new List<IEventEffect> { new GainCoin(50) }),
+                    new EventOption("Plunder the mercenary group", "Gain 300 coin, Lose 20 health(All Party)", new List<IEventEffect> { new GainCoin(300), new HurtUnit(20, true) }),
+                    CreateOption()
+                }
+            ),
+            new GameEvent
+            (
+                new List<EventOption>
+                {
+                    new EventOption("Offer a prayer to God", "Increase defense by 5(All Party)", new List<IEventEffect> { new UpgradeDefense(5) }),
+                    new EventOption("Offer tribute", "Pay 100 coin, Increase maxhealth by 5(All Party)", new List<IEventEffect> { new PayCoin(100), new UpgradeHealth(5) }),
+                    new EventOption("Pledge a blood oath.", "Lose 10 health(All Party), Increase attack by 5(All Party)", new List<IEventEffect> { new HurtUnit(10, true), new UpgradeAttack(5) }),
+                }
+            ),
+            new GameEvent
+            (
+                new List<EventOption>
+                {
+                    new EventOption("Heal it", "Pay 100 coin, Get 1 random card", new List<IEventEffect> { new PayCoin(100), new GetRandomCard(1) }),
+                    new EventOption("Kill and plunder", "Decrease maxhealth by 5(All Party), Get 1 random card", new List<IEventEffect> { new DecreaseHealth(5), new GetRandomCard(1) }),
+                    new EventOption("Abandon and leave", "Nothing happens", new List<IEventEffect> { }),
+                }
+            ),
             new GameEvent
             (
                 new List<EventOption>
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -62,9 +62,9 @@ public class GameManager : MonoBehaviour
         _maxChapter = _map.MaxChapter;
         TownRestManager.Instance.CardDeleteCoin = 100;
 
-        _playerUnitDatas.Add(new UnitData(DataManager.Instance.UnitDatas[0]));
+        //_playerUnitDatas.Add(new UnitData(DataManager.Instance.UnitDatas[0]));
         //_playerUnitDatas.Add(new UnitData(DataManager.Instance.UnitDatas[1]));
-        _playerUnitDatas.Add(new UnitData(DataManager.Instance.UnitDatas[2]));
+        //_playerUnitDatas.Add(new UnitData(DataManager.Instance.UnitDatas[2]));
         //_playerUnitDatas.Add(new UnitData(DataManager.Instance.UnitDatas[3]));
 
         _playerCardDatas.Add(new CardData(DataManager.Instance.CardDatas[0]));
@@ -78,12 +78,6 @@ public class GameManager : MonoBehaviour
         _playerCardDatas.Add(new CardData(DataManager.Instance.CardDatas[7]));
         _playerCardDatas.Add(new CardData(DataManager.Instance.CardDatas[8]));
         _playerCardDatas.Add(new CardData(DataManager.Instance.CardDatas[9]));
-        _playerCardDatas.Add(new CardData(DataManager.Instance.CardDatas[10]));
-        _playerCardDatas.Add(new CardData(DataManager.Instance.CardDatas[10]));
-        _playerCardDatas.Add(new CardData(DataManager.Instance.CardDatas[10]));
-        _playerCardDatas.Add(new CardData(DataManager.Instance.CardDatas[10]));
-        _playerCardDatas.Add(new CardData(DataManager.Instance.CardDatas[10]));
-        _playerCardDatas.Add(new CardData(DataManager.Instance.CardDatas[10]));
         _playerCardDatas.Add(new CardData(DataManager.Instance.CardDatas[10]));
 
         StartGame();
